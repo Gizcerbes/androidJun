be1f19465059d47f52bc44189dffecdd061638be
gizcerbes@outlook.com <278152andr3328>
2022-06-28 01:45:35 +0300

    Behavior

--- app/src/main/java/com/uogames/androidjun/HomeFragment.kt
+++ app/src/main/java/com/uogames/androidjun/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.uogames.androidjun
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,24 +11,49 @@ import com.uogames.androidjun.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-	private lateinit var bind : FragmentHomeBinding
+    private lateinit var bind: FragmentHomeBinding
 
-	private lateinit var bottomSheet: BottomSheetBehavior<View>
+    private lateinit var bottomSheet: BottomSheetBehavior<View>
 
-	override fun onCreateView(
-		inflater: LayoutInflater,
-		container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View {
-		bind = FragmentHomeBinding.inflate(inflater, container, false)
-		return bind.root
-	}
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bind = FragmentHomeBinding.inflate(inflater, container, false)
+        return bind.root
+    }
 
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		bottomSheet =BottomSheetBehavior.from(bind.cardBehavior)
-		bottomSheet.isFitToContents = false
-		bottomSheet.halfExpandedRatio = 0.6f
-	}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        bottomSheet = BottomSheetBehavior.from(bind.cardBehavior)
+        bottomSheet.isFitToContents = false
+        bottomSheet.halfExpandedRatio = 0.6f
 
+        bind.hider.visibility = View.GONE
+
+
+        bind.btn.setOnClickListener {
+            Log.e("TAG", "btn: ", )
+        }
+
+        bottomSheet.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                bind.hider.visibility =
+                    if (newState == BottomSheetBehavior.STATE_COLLAPSED) View.GONE else View.VISIBLE
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
+        bind.cardBehavior.setOnClickListener {
+            Log.e("TAG", "cardBehavior: ", )
+        }
+
+        bind.hider.setOnClickListener {
+            bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+
+
+    }
 
 }
